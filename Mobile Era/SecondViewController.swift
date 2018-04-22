00275ff46b60e674e062fe84e0b7960fbeb61130
Konstantin Loginov <loginov.k@gmail.com>
2018-04-22 08:36:36 +0200

    Initial Commit

---
+++ Mobile Era/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mobile Era
+//
+//  Created by Konstantin Loginov on 22/04/2018.
+//  Copyright © 2018 FotMob. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
